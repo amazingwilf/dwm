b532c4602f3f074f8a212a24ee8d82f2f09632d9
David Elliott <david@starjump.org>
2024-12-10 18:25:12 +0000

    hidevacanttags

--- config.def.h
+++ config.def.h
@@ -47,7 +47,8 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8" }; */
+static char *tags[]	= { " ", " ", " ", " ", " ", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
