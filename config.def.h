74765475f03a1fc31e1bb2baccc57ec1baa14e21
David Elliott <david@starjump.org>
2025-06-17 13:38:12 +0100

    xrdb

--- config.def.h
+++ config.def.h
@@ -1,26 +1,31 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const unsigned int borderpx		= 2;
+static const unsigned int snap			= 32;
+
+static const int showbar				= 1;
+static const int topbar					= 1;
+
+static const char *fonts[]				= { "NotoSans Nerd Font:size=11" };
+static const char dmenufont[]			= "NotoSans Nerd Font:size=11";
+
+static char normfgcolor[]				= "#bbbbbb";
+static char normbgcolor[]				= "#222222";
+static char normbordercolor[]			= "#444444";
+
+static char selfgcolor[]				= "#eeeeee";
+static char selbgcolor[]				= "#005577";
+static char selbordercolor[]			= "#005577";
+
+static char *colors[][3]				= {
+       [SchemeNorm] = { normfgcolor,	normbgcolor,	normbordercolor },
+       [SchemeSel]  = { selfgcolor,		selbgcolor,		selbordercolor },
 };
 
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
-static const unsigned int alphas[][3]      = {
+static const unsigned int baralpha		= 0xd0;
+static const unsigned int borderalpha	= OPAQUE;
+static const unsigned int alphas[][3]	= {
     /*               fg      bg        border*/
     [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
@@ -93,6 +98,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_x,      xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
