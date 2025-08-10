cae62f918d2e0dd6e94119940047ca880692d2e2
David Elliott <david@starjump.org>
2025-08-10 22:01:25 +0100

    bar-height

--- config.def.h
+++ config.def.h
@@ -12,6 +12,7 @@ static const unsigned int gappov    = 15;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 10;        /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "DejaVu Sans:style=Book:size=12",
 										"JetBrainsMono Nerd Font:style=ExtraBold:size=10" };
 
