1ee63ad73c5e28078959bed744b923b16974c6da
David Elliott <david@starjump.org>
2025-08-27 21:28:42 +0100

    Tweaks

---
+++ colors.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]	= "#0d0d0d";
+static const char col_gray2[]	= "#282c34";
+static const char col_gray3[]	= "#abb2bf";
+static const char col_gray4[]	= "#dcdfe4";
+static const char col_blue[]	= "#41afef";
+static const char col_title[]	= "#000090";
--- config.def.h
+++ config.def.h
@@ -11,16 +11,11 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Adwaita Sans:size=11" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+#include "colors.h"
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_title,  col_blue  },
 };
 
 /* tagging */
@@ -77,8 +72,6 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "ghostty", NULL };
 static const char *roficmd[]	= { "rofi", "-show", "drun", NULL };
 
