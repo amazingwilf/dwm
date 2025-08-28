9af838ce723ce3170f46387a28a44b7cb2a79b98
David Elliott <david@starjump.org>
2025-08-28 20:08:09 +0100

    scratchpads

--- config.def.h
+++ config.def.h
@@ -1,22 +1,34 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Adwaita Sans:size=10",
-										"JetBrainsMono Nerd Font:style=ExtraBold:size=9" };
+static const unsigned int borderpx	= 2; 
+static const unsigned int snap		= 32;
+static const unsigned int gappih	= 10;
+static const unsigned int gappiv	= 10;
+static const unsigned int gappoh	= 15;
+static const unsigned int gappov	= 15;
+static       int smartgaps			= 0;
+static const int showbar			= 1;
+static const int topbar				= 1;
+static const char *fonts[]			= { "Adwaita Sans:size=10",
+										"JetBrainsMono Nerd Font:style=ExtraBold:size=8" };
 #include "colors.h"
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3,	col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_title,  col_blue  },
+};
+
+typedef struct {
+	const char *name;
+	const void *cmd;
+} Sp;
+
+const char *spcmd1[] = {"ghostty", "--x11-instance-name=spterm", NULL };
+const char *spcmd2[] = {"ghostty", "--x11-instance-name=spfm", "--initial-command=ranger", NULL };
+static Sp scratchpads[] = {
+	/* name          cmd  */
+	{"spterm",      spcmd1},
+	{"spranger",    spcmd2},
 };
 
 /* tagging */
@@ -27,20 +39,21 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ .class = "firefox", .tags = 1 << 1 },
+	{ .class = "Nwg-look", .isfloating = 1 },
+	{ .instance = "spterm", .tags = SPTAG(0), .isfloating = 1},
+	{ .instance = "spfm", .tags = SPTAG(1), .isfloating = 1},
 };
 
 /* layout(s) */
-static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
-static const int attachmode         = 2;        /* 0 master (default), 1 = above, 2 = aside, 3 = below, 4 = bottom */
+static const float mfact		= 0.50;
+static const int nmaster		= 1; 
+static const int resizehints	= 0;
+static const int lockfullscreen	= 1;
+static const int refreshrate	= 120;
+static const int attachmode		= 2; 
 
-#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#define FORCE_VSPLIT 1 
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
@@ -66,13 +79,17 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[]  = { "ghostty", NULL };
+static const char *termcmd[]	= { "ghostty", NULL };
 static const char *roficmd[]	= { "rofi", "-show", "drun", NULL };
+static const char *firefoxcmd[]	= { "firefox", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = firefoxcmd } },
+	{ MODKEY,            			XK_y,  	   togglescratch,  {.ui = 0 } },
+	{ MODKEY,            			XK_u,	   togglescratch,  {.ui = 1 } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -120,7 +137,7 @@ static const Button buttons[] = {
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY,         Button1,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
